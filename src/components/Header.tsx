a4aa9a63d95df51fa4ae7d7eb14eebbe22099e7c
Doug <douglashorville@gmail.com>
2023-05-16 17:15:47 -0400

    Header update; add Navbar component

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,10 +1,34 @@
 import React from 'react';
 
+import {AppBar, Toolbar, IconButton} from '@mui/material';
+import FacebookSharpIcon from '@mui/icons-material/FacebookSharp';
+import PhoneSharpIcon from '@mui/icons-material/PhoneSharp';
+
 const Header = () => {
   return (
-    <>
-      header
-    </>
+
+    <AppBar>
+      <Toolbar>
+        <div className='header_st'>
+          INDUSTRIAL REFRIGERATION EXPERTS
+        </div>
+        <div className='header_social'>
+          {/* Icon Button requires these attributes */}
+          <IconButton aria-label='Menu' color='inherit'>
+            <FacebookSharpIcon />
+          </IconButton>
+        </div>
+        <div className='header_phone'>
+        <IconButton aria-label='Menu' color='inherit'>
+            <PhoneSharpIcon />
+          </IconButton>
+          (908) 999-1234
+        </div>
+        
+      </Toolbar>
+    </AppBar>
+    
+
   )
 }
 
---
+++ src/components/Navbar.tsx
@@ -0,0 +1,25 @@
+import React from 'react';
+
+import {AppBar, Toolbar, IconButton} from '@mui/material';
+import MenuIcon from '@mui/icons-material/Menu';
+
+const Navbar = () => {
+  return (
+
+    <AppBar>
+      <Toolbar>
+        <div className='header_logo'>
+          ?
+        </div>
+        {/* Icon Button requires these attributes */}
+        <IconButton aria-label='Menu' color='inherit'>
+          <MenuIcon />
+        </IconButton>
+      </Toolbar>
+    </AppBar>
+    
+
+  )
+}
+
+export default Navbar;
